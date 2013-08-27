759478eeab53cd93c8bb0aff292a2e9181ba922d
Nathanael Jones <nathanael.jones@gmail.com>
2013-08-27 12:55:51 -0400

    Failed attempt at passing options to the HTML adapters.

--- lib/repositext/kramdown_adaptions.rb
+++ lib/repositext/kramdown_adaptions.rb
@@ -5,15 +5,22 @@ require 'kramdown/converter/html'
 class Kramdown::Converter::Html
 
   def convert_subdoc(el, indent)
-    format_as_indented_block_html('div', el.attr, inner(el, indent), indent)
+    format_as_indented_block_html('div', el.attr, inner(el, indent), indent) unless @options[:disable_subdoc]
   end
 
   def convert_line_synchro_marker(el, indent)
-    "<span class=\"sync-mark-b\"></span>"
+    "<span class=\"sync-mark-b\"></span>" unless @options[:disable_sync_b]
   end
 
   def convert_word_synchro_marker(el, indent)
-    "<span class=\"sync-mark-a\"></span>"
+    "<span class=\"sync-mark-a\"></span>" unless @options[:disable_sync_a]
   end
+
+  alias_method :convert_root_old, :convert_root
+  def convert_root(el, indent)
+    "<!-- #{Time.now.to_s} -->\n" + 
+    "<!-- #{@options.inspect} -->\n" + 
+    convert_root_old(el,indent)
+  end 
 
 end
