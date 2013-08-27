6021f55a2e96a7c4322852bc57d218175a99faa5
Nathanael Jones <nathanael.jones@gmail.com>
2013-08-27 15:53:05 -0400

    Added support for dropping AT features individually during export, thanks to Thomas' help

--- lib/repositext/kramdown_adaptions.rb
+++ lib/repositext/kramdown_adaptions.rb
@@ -1,19 +1,26 @@
 # -*- encoding: utf-8 -*-
 
 require 'kramdown/converter/html'
+require 'kramdown/options'
+
+module Kramdown::Options
+  define(:disable_sync_a, Boolean, false, "Some documentation for the option") 
+  define(:disable_sync_b, Boolean, false, "Some documentation for the option") 
+  define(:disable_subdoc, Boolean, false, "Some documentation for the option") 
+end
 
 class Kramdown::Converter::Html
 
   def convert_subdoc(el, indent)
-    format_as_indented_block_html('div', el.attr, inner(el, indent), indent) unless @options[:disable_subdoc]
+    @options[:disable_subdoc] ? inner(el, indent) : format_as_indented_block_html('div', el.attr, inner(el, indent), indent) 
   end
 
   def convert_line_synchro_marker(el, indent)
-    "<span class=\"sync-mark-b\"></span>" unless @options[:disable_sync_b]
+    @options[:disable_sync_b] ? "" : "<span class=\"sync-mark-b\"></span>"
   end
 
   def convert_word_synchro_marker(el, indent)
-    "<span class=\"sync-mark-a\"></span>" unless @options[:disable_sync_a]
+    @options[:disable_sync_a] ? "" : "<span class=\"sync-mark-a\"></span>"  
   end
 
   alias_method :convert_root_old, :convert_root
