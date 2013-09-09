80b27a99ddece984beda032f8a418acc2759d238
Thomas Leitner <t_leitner@gmx.at>
2013-09-09 17:38:58 +0200

    Added correct category to custom elements

--- lib/repositext/parser.rb
+++ lib/repositext/parser.rb
@@ -34,7 +34,7 @@ module Kramdown
       # Parse the document divider at the current location
       def parse_document_divider
         if @src.scan(DOCUMENT_DIVIDER)
-          @tree = el = new_block_el(:subdoc)
+          @tree = el = new_block_el(:subdoc, nil, nil, :category => :block)
           parse_attribute_list(@src[2], el.options[:ial] ||= Utils::OrderedHash.new) if @src[1]
           @root.children << el
           true
@@ -50,7 +50,7 @@ module Kramdown
       # Parse word synchronization marker at current location.
       def parse_word_synchro_marker
         @src.pos += @src.matched_size
-        @tree.children << Element.new(:word_synchro_marker)
+        @tree.children << Element.new(:word_synchro_marker, nil, nil, :category => :span)
       end
       define_parser(:word_synchro_marker, WORD_SYNCHRO_MARKER, WORD_SYNCHRO_MARKER)
 
@@ -60,7 +60,7 @@ module Kramdown
       # Parse line synchronization marker at current location.
       def parse_line_synchro_marker
         @src.pos += @src.matched_size
-        @tree.children << Element.new(:line_synchro_marker)
+        @tree.children << Element.new(:line_synchro_marker, nil, nil, :category => :span)
       end
       define_parser(:line_synchro_marker, LINE_SYNCHRO_MARKER, LINE_SYNCHRO_MARKER)
 
