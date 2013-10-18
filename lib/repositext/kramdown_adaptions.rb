9d44fb5e9db72368598bd9be2167d146f4d19cfc
Jo Hund <jhund@clearcove.ca>
2013-10-18 09:25:13 -0700

    Defined more Kramdown::Options for validating parsers.

--- lib/repositext/kramdown_adaptions.rb
+++ lib/repositext/kramdown_adaptions.rb
@@ -4,10 +4,16 @@ require 'kramdown/converter/html'
 require 'kramdown/converter/kramdown'
 require 'kramdown/options'
 
+# We need to define options since Kramdown currently discards any unknown options
+# in Kramdown::Options.merge.
+# This may change in the future.
 module Kramdown::Options
   define(:disable_subtitle_mark, Boolean, false, "Some documentation for the option")
   define(:disable_gap_mark, Boolean, false, "Some documentation for the option")
   define(:disable_record_mark, Boolean, false, "Some documentation for the option")
+  define(:validation_errors, Object, nil, "Some documentation for the option") { |v| v }
+  define(:validation_file_descriptor, String, '', "Some documentation for the option")
+  define(:validation_warnings, Object, nil, "Some documentation for the option") { |v| v }
 end
 
 class Kramdown::Converter::Html
