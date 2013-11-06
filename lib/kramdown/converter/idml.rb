f36584c70b0f9ecfabdbdbf069fff366cb06f5ca
Jo Hund <jhund@clearcove.ca>
2013-11-06 12:44:57 -0800

    Some tweaks to IDML converter (WIP)

--- lib/kramdown/converter/idml.rb
+++ lib/kramdown/converter/idml.rb
@@ -28,8 +28,7 @@ module Kramdown
 
       def configure_rubyzip
         Zip.setup do |c|
-          c.on_exists_proc = true
-          c.continue_on_exists_proc = true
+          c.continue_on_exists_proc = true # overwrite existing ZIP archives when compressing
           c.unicode_names = true
         end
       end
@@ -38,7 +37,8 @@ module Kramdown
       # @param[Kramdown::Element] root the root element
       # @return[String] the story XML as string
       def compute_story_xml(root)
-        xml_string = Kramdown::Converter::IdmlStory.convert(root, @options)
+        xml_string, _warnings = ::Kramdown::Converter::IdmlStory.convert(root, @options)
+        xml_string
       end
 
       # Builds array of all files to go into ZIP archive
@@ -46,7 +46,24 @@ module Kramdown
       # @return[Array<Array>] Array of tuples (file_name and file_contents)
       def collect_files_for_archive(story_xml)
         [
-          ['theStory', story_xml]
+          ['mimetype', 'application/vnd.adobe.indesign-idml-package'],
+          # META-INF is optional
+          # http://wwwimages.adobe.com/www.adobe.com/content/dam/Adobe/en/devnet/indesign/sdk/cs6/idml/idml-specification.pdf
+          # Appendix A, 1.4.6 (p. 401)
+          ['META-INF/container.xml', ""],
+          ['META-INF/metadata.xml', ""],
+          ['designmap.xml', ""],
+          ['MasterSpreads/MasterSpread_A.xml', ""],
+          ['Resources/Fonts.xml', ""],
+          ['Resources/Graphic.xml', ""],
+          ['Resources/Preferences.xml', ""],
+          ['Resources/Styles.xml', ""],
+          ['Spreads/Spread_spread1.xml', ""],
+          ['Spreads/Spread_spread2.xml', ""],
+          ['Stories/Story_story0.xml', story_xml],
+          ['XML/BackingStory.xml', ""],
+          ['XML/Mapping.xml', ""],
+          ['XML/Tags.xml', ""]
         ]
       end
 
@@ -54,10 +71,10 @@ module Kramdown
       # @param[String] zip_archive_file_name a complete path to the output file location and name
       # @param[Array<Array>] files the files to include in archive. Tuple of name and contents.
       def create_zip_archive(zip_archive_file_name, files)
-        Zip::OutputStream.open(zip_archive_file_name) { |zos|
+        Zip::OutputStream.open(zip_archive_file_name) { |io|
           files.each do |(file_name, contents)|
-            zos.put_next_entry(file_name)
-            zos.puts contents
+            io.put_next_entry(file_name)
+            io.write(contents)
           end
         }
       end
