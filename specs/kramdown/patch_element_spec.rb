35afa177cd5f2ab2bcd33a32e9e48c3ec3f4c40e
Jo Hund <jhund@clearcove.ca>
2013-11-19 13:29:34 -0800

    Update specs to work with kramdown's new location information.

--- specs/kramdown/patch_element_spec.rb
+++ specs/kramdown/patch_element_spec.rb
@@ -5,15 +5,15 @@ describe Kramdown::Element do
   describe "inspect_tree" do
     it "prints a representation of element's tree" do
       doc = Kramdown::Document.new("para *one*\n\npara _two_", { :input => :repositext })
-      lambda { doc.root.inspect_tree }.must_output %( - :root - {:encoding=>#<Encoding:UTF-8>, :abbrev_defs=>{}}
-   - :p
+      lambda { doc.root.inspect_tree }.must_output %( - :root - {:encoding=>#<Encoding:UTF-8>, :location=>1, :abbrev_defs=>{}}
+   - :p - {:location=>1}
      - :text - "para "
-     - :em
+     - :em - {:location=>1}
        - :text - "one"
    - :blank - "\\n"
-   - :p
+   - :p - {:location=>3}
      - :text - "para "
-     - :em
+     - :em - {:location=>3}
        - :text - "two"
 )
     end
