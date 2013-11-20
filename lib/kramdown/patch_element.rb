346b7f980d987a632b574310406bc9d6213e3fc3
Jo Hund <jhund@clearcove.ca>
2013-11-20 13:21:25 -0800

    Change API for Kramdown::Element#inspect_tree: now returns a string instead of using puts.

--- lib/kramdown/patch_element.rb
+++ lib/kramdown/patch_element.rb
@@ -3,9 +3,11 @@ module Kramdown
 
     # Prints a tree representation of self, including all descendants. Uses
     # `puts` to print output.
+    # @param[String, optional] output all output is collected recursively into this string
     # @param[Integer, optional] _indent_level used for recursion
     # @param[Hash, optional] _options
-    def inspect_tree(_indent_level = 0, _options = {})
+    # @return[String]
+    def inspect_tree(output = '', _indent_level = 0, _options = {})
       el_options = {
         :max_value_length => 80,
         :indent => '  '
@@ -20,27 +22,30 @@ module Kramdown
           el_value = "#{ el_value[0..half_len] }...#{ el_value[(-1 * half_len)..-1] }"
         end
         el_value = el_value.length > 0 ? el_value.inspect : nil
-        puts [
+        output << [
           el_indent,
           ":#{type}",
           (attr.inspect  if attr && attr.any?),
           (options.inspect  if options && options.any?),
           el_value
         ].compact.join(" - ")
+        output << "\n"
       when Kramdown::Element
-        el_value.inspect_tree(_indent_level + 1, el_options)
+        el_value.inspect_tree(output, _indent_level + 1, el_options)
       when Kramdown::Utils::Entities::Entity
-        puts [
+        output [
           el_indent,
           ":#{type}",
           (attr.inspect  if attr && attr.any?),
           (options.inspect  if options && options.any?),
           "code_point: #{el_value.code_point}"
         ].compact.join(" - ")
+        output << "\n"
       else
         raise el_value.inspect
       end
-      children.each { |e| e.inspect_tree(_indent_level + 1, el_options) }
+      children.each { |e| e.inspect_tree(output, _indent_level + 1, el_options) }
+      output
     end
 
   end
