098df953116c740c77ec5f633d94cb7bbe9de404
Jo Hund <jhund@clearcove.ca>
2013-12-09 11:26:50 -0800

    Break plain_text convert specs into two parts, one using kramdown proper, the other using repositext-kramdown.

--- specs/kramdown/converter/plain_text_spec.rb
+++ specs/kramdown/converter/plain_text_spec.rb
@@ -2,16 +2,35 @@ require_relative '../../helper'
 
 describe Kramdown::Converter::PlainText do
 
+  # Specs that run with repositext-kramdown because :
+  # a) repositext-kramdown can handle the element type OR
+  # b) repositext-kramdown's behavior is different from kramdown proper
   [
     [%(the body), %(the body)],
-    [%([the link](link.html)), %(the link)],
     [%(# the header), %(the header)],
-    [%(* list 1\n* list 2\n* list 3), %(list 1\nlist 2\nlist 3)],
     [%(*the em*), %(the em)],
     [%(**the strong**), %(the strong)],
     [%(para 1\n\npara 2), %(para 1\n\npara 2)],
     [
+      %(# header 1\n\n## header 2 *with em*\n\npara 1\n\npara 2 **with strong**.),
+      %(header 1\n\nheader 2 with em\n\npara 1\n\npara 2 with strong.)
+    ],
     [%(some text with \\[escaped brackets\\]), %(some text with [escaped brackets])],
+  ].each_with_index do |(kramdown, expected), idx|
+    it "converts example #{ idx + 1 } to plain text" do
+      doc = Kramdown::Document.new(
+        kramdown, { :input => 'repositext' }
+      )
+      doc.to_plain_text.must_equal expected
+    end
+  end
+
+  # Specs that need to run with kramdown proper (because repositext-kramdown
+  # doesn't support these elements (yet).)
+  [
+    [%([the link](link.html)), %(the link)],
+    [%(* list 1\n* list 2\n* list 3), %(list 1\nlist 2\nlist 3)],
+    [
       %(# header 1\n\n## header 2 *with em*\n\npara 1\n\npara 2 **with strong** and [link](link.html).),
       %(header 1\n\nheader 2 with em\n\npara 1\n\npara 2 with strong and link.)
     ],
