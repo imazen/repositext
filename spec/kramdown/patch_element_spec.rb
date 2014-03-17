f2228520ab8567980613dd8fd55b0592c2fa33f5
Jo Hund <jhund@clearcove.ca>
2014-03-17 12:38:29 -0700

    Updated kramdown/patch_element_spec: improved test data, stop using Kramdown::ElementRt, use plain Kramdown::Element instead.

--- spec/kramdown/patch_element_spec.rb
+++ spec/kramdown/patch_element_spec.rb
@@ -2,13 +2,13 @@ require_relative '../helper'
 
 describe Kramdown::Element do
 
-  describe "inspect_tree" do
+  describe "#inspect_tree" do
     it "prints a representation of element's tree" do
-      doc = Kramdown::Document.new("para *one*\n\npara _two_", { :input => 'KramdownRepositext' })
+      doc = Kramdown::Document.new("para *one*{: .klass}\n\npara _two_", { :input => 'KramdownRepositext' })
       doc.root.inspect_tree.must_equal %( - :root - {:encoding=>#<Encoding:UTF-8>, :location=>1, :abbrev_defs=>{}}
    - :p - {:location=>1}
      - :text - {:location=>1} - \"para \"
-     - :em - {:location=>1}
+     - :em - {\"class\"=>\"klass\"} - {:location=>1, :ial=>{:refs=>[nil], \"class\"=>\"klass\"}}
        - :text - {:location=>1} - \"one\"
    - :blank - {:location=>3} - \"\\n\"
    - :p - {:location=>3}
@@ -30,8 +30,8 @@ describe Kramdown::Element do
       [[:text, 'text1'], [:text, 'text2'], true]
     ].each_with_index do |(first_params, second_params, xpect), i|
       it "handles scenario #{ i + 1 }" do
-        ke1 = Kramdown::ElementRt.new(*first_params)
-        ke2 = Kramdown::ElementRt.new(*second_params)
+        ke1 = Kramdown::Element.new(*first_params)
+        ke2 = Kramdown::Element.new(*second_params)
         ke1.is_of_same_type_as?(ke2).must_equal xpect
         ke2.is_of_same_type_as?(ke1).must_equal xpect
       end
