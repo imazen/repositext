78902c3946f90b4ab47f8fcfec45247269f2a2d1
Jo Hund <jhund@clearcove.ca>
2014-03-26 14:45:04 -0700

    Don't customize :escaped_chars span parser. This will fix part of the issues where we get escaped brackets and colons in at kramdown files.

--- lib/kramdown/parser/kramdown_repositext.rb
+++ lib/kramdown/parser/kramdown_repositext.rb
@@ -4,7 +4,6 @@ require 'kramdown/parser/kramdown'
 
 module Kramdown
   module Parser
-
     class KramdownRepositext < Kramdown
 
       # Create a new Kramdown parser object with the given +options+.
@@ -13,21 +12,16 @@ module Kramdown
 
         @block_parsers = [:blank_line, :atx_header, :horizontal_rule, :setext_header,
                           :block_extensions, :paragraph]
-        @span_parsers =  [:emphasis, :span_extensions, :line_break,
-                          :repositext_escaped_chars]
+        @span_parsers =  [:emphasis, :span_extensions, :line_break, :escaped_chars]
       end
 
+      # @return[Array] array with [root, [warnings]]
       def parse #:nodoc:
         configure_parser
         parse_blocks(@root, adapt_source(source))
         update_tree(@root)
       end
 
-      # original: ESCAPED_CHARS = /\\([\\.*_+`<>()\[\]{}#!:|"'\$=-])/
-      REPOSITEXT_ESCAPED_CHARS = /\\([\\#*_{=@%-])/
-      define_parser(:repositext_escaped_chars, REPOSITEXT_ESCAPED_CHARS, '\\\\', :parse_escaped_chars)
-
     end
-
   end
 end
