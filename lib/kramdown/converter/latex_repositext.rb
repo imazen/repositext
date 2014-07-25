9ed943a4521bcb5d238d4c4bba1c66656ccbecaa
Jo Hund <jhund@clearcove.ca>
2014-07-24 17:33:49 -0700

    Work around for bold/bold-italic issue with V-Calisto-St and latex: We have to reverse usage. I.e., whenever we want bold, we specify bold-italic, and vice versa.

--- lib/kramdown/converter/latex_repositext.rb
+++ lib/kramdown/converter/latex_repositext.rb
@@ -177,6 +177,13 @@ module Kramdown
         wrap_body_in_template(latex_body, document_title)
       end
 
+      # NOTE: There is a strange bug where in the V-Calisto-St font and latex
+      # bold and bold-italic are reversed. So whenever I want bold, I have to
+      # specify bold-italic and vice versa.
+      def convert_strong(el, opts)
+        "\\textit{\\textbf{#{ inner(el, opts) }}}"
+      end
+
       # Override this method in any subclasses that render subtitle_marks
       def convert_subtitle_mark(el, opts)
         ''
