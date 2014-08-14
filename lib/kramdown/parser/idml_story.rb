c35d0e303187a7cdae704c2959c8d0910b436701
Jo Hund <jhund@clearcove.ca>
2014-08-14 15:21:35 -0700

    Add line location on idml_story parser exception

--- lib/kramdown/parser/idml_story.rb
+++ lib/kramdown/parser/idml_story.rb
@@ -143,7 +143,7 @@ module Kramdown
           when 'Properties'
             # ignore
           else
-            raise InvalidElementException, "Found unexpected child element #{child.name} of ParagraphStyleRange"
+            raise InvalidElementException, "Found unexpected child element #{child.name} of ParagraphStyleRange on line #{ child.line }"
           end
         end
       end
@@ -324,7 +324,7 @@ module Kramdown
           else
             raise(
               InvalidElementException,
-              "Found unexpected child element #{child.name} of CharacterStyleRange"
+              "Found unexpected child element #{ child.name } of CharacterStyleRange on line #{ child.line }"
             )
           end
         end
