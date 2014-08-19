a72fb7553bfc631440f3a97664a0557207c5aeea
Jo Hund <jhund@clearcove.ca>
2014-08-18 18:38:38 -0700

    Updated documentation

--- lib/repositext/utils/entity_encoder.rb
+++ lib/repositext/utils/entity_encoder.rb
@@ -4,6 +4,8 @@ class Repositext
     class EntityEncoder
 
       def self.encode(text)
+        # Encode the unicode codepoints that are allowed per
+        # Repositext::Validation::Config::INVALID_CODE_POINTS
         text.gsub(/[\u00A0\u2011\u2028\u202F\uFEFF]/) { |match|
           sprintf('&#x%04X;', match.codepoints.first)
         }
