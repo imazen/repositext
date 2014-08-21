11a88064b179b923b6618ff66d7327cefa816490
Jo Hund <jhund@clearcove.ca>
2014-08-21 10:54:13 -0700

    Changed record_id format from "#f-..." to "#rid-..."

--- spec/repositext/fix/adjust_merged_record_mark_positions_spec.rb
+++ spec/repositext/fix/adjust_merged_record_mark_positions_spec.rb
@@ -5,23 +5,23 @@ describe Repositext::Fix::AdjustMergedRecordMarkPositions do
   [
     [
       "Moves :record_mark to before para 1",
-      %(\n\nword\n^^^ {: .rid #f-58660019 kpn="001"}\n %word word word word \n\n),
-      %(^^^ {: .rid #f-58660019 kpn="001"}\n\nword %word word word word \n\n),
+      %(\n\nword\n^^^ {: .rid #rid-58660019 kpn="001"}\n %word word word word \n\n),
+      %(^^^ {: .rid #rid-58660019 kpn="001"}\n\nword %word word word word \n\n),
     ],
     [
       "Moves :record_mark to before para 2",
-      %(# *word word word*\n\nword\n^^^ {: .rid #f-54250019 kpn="001"}\n …-%word word word\n\n),
-      %(# *word word word*\n\n^^^ {: .rid #f-54250019 kpn="001"}\n\nword …-%word word word\n\n),
+      %(# *word word word*\n\nword\n^^^ {: .rid #rid-54250019 kpn="001"}\n …-%word word word\n\n),
+      %(# *word word word*\n\n^^^ {: .rid #rid-54250019 kpn="001"}\n\nword …-%word word word\n\n),
     ],
     [
       "Moves :record_mark to after para 1",
-      %(# *word word word*\n\nword word word word\n^^^ {: .rid #f-54250019 kpn="001"}\n …-%word word\n\n),
-      %(# *word word word*\n\nword word word word …-%word word\n\n^^^ {: .rid #f-54250019 kpn="001"}\n\n),
+      %(# *word word word*\n\nword word word word\n^^^ {: .rid #rid-54250019 kpn="001"}\n …-%word word\n\n),
+      %(# *word word word*\n\nword word word word …-%word word\n\n^^^ {: .rid #rid-54250019 kpn="001"}\n\n),
     ],
     [
       "Moves :record_mark symmetrically between two paragraphs",
-      %(word word\n{: .normal_pn}\n\n\n^^^ {: .rid #f-54250049 kpn="004"}\n*11*{: .pn} %word word),
-      %(word word\n{: .normal_pn}\n\n^^^ {: .rid #f-54250049 kpn="004"}\n\n*11*{: .pn} %word word),
+      %(word word\n{: .normal_pn}\n\n\n^^^ {: .rid #rid-54250049 kpn="004"}\n*11*{: .pn} %word word),
+      %(word word\n{: .normal_pn}\n\n^^^ {: .rid #rid-54250049 kpn="004"}\n\n*11*{: .pn} %word word),
     ],
   ].each do |(desc, txt, xpect)|
     it desc do
@@ -61,7 +61,7 @@ describe Repositext::Fix::AdjustMergedRecordMarkPositions do
   describe '.make_sure_there_is_a_blank_line_after_first_record_mark' do
     [
       ["^^^\nsimple case", "^^^\n\nsimple case"],
-      ["^^^ {: .rid #f-59050019 kpn=\"001\"}\nwith ial", "^^^ {: .rid #f-59050019 kpn=\"001\"}\n\nwith ial"],
+      ["^^^ {: .rid #rid-59050019 kpn=\"001\"}\nwith ial", "^^^ {: .rid #rid-59050019 kpn=\"001\"}\n\nwith ial"],
     ].each do |(txt, xpect)|
       it "modifies '#{ txt }'" do
         Repositext::Fix::AdjustMergedRecordMarkPositions.make_sure_there_is_a_blank_line_after_first_record_mark(
@@ -72,7 +72,7 @@ describe Repositext::Fix::AdjustMergedRecordMarkPositions do
 
     [
       " ^^^\nrecord_mark_not_at_beginning_of doc",
-      "^^^ {: .rid #f-59050019 kpn=\"001\"}\n\nalready has two newlines",
+      "^^^ {: .rid #rid-59050019 kpn=\"001\"}\n\nalready has two newlines",
     ].each do |txt|
       it "does not modify '#{ txt }'" do
         Repositext::Fix::AdjustMergedRecordMarkPositions.make_sure_there_is_a_blank_line_after_first_record_mark(
