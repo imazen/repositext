8965cef97eed4e9c84be53d82469af0dcafd4d03
Jo Hund <jhund@clearcove.ca>
2014-10-02 10:47:42 -0700

    Updated AdjacentElementMerger to handle :entity elements

--- lib/kramdown/mixins/adjacent_element_merger.rb
+++ lib/kramdown/mixins/adjacent_element_merger.rb
@@ -28,6 +28,7 @@ module Kramdown
       if(
         ke.children.empty? || # nothing to merge
         :block == ::Kramdown::Element.category(ke.children.first) || # can't merge paras or headers
+        :entity == ke.type # can't merge entities. They are atomic.
         :root == ke.type
       )
         return nil
