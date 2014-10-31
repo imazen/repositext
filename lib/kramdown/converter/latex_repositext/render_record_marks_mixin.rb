61f2a8d9f728c07a542d25a5366e71830b147d94
Jo Hund <jhund@clearcove.ca>
2014-10-31 11:25:30 -0700

    Remove another instance of break_out_of_song (not needed any more)

--- lib/kramdown/converter/latex_repositext/render_record_marks_mixin.rb
+++ lib/kramdown/converter/latex_repositext/render_record_marks_mixin.rb
@@ -5,14 +5,12 @@ module Kramdown
       module RenderRecordMarksMixin
 
         def convert_record_mark(el, opts)
-          r = break_out_of_song(@inside_song)
           if @options[:disable_record_mark]
-            r << inner(el, opts)
+            inner(el, opts)
           else
             meta = %( id: #{ el.attr['id'] })
-            r << "\\RtRecordMark{#{ meta }}#{ inner(el, opts) }"
+            "\\RtRecordMark{#{ meta }}#{ inner(el, opts) }"
           end
-          r
         end
 
       end
