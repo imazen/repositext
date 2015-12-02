18b60b5270c6c678902d4490e5684d3f4779017e
Jo Hund <jhund@clearcove.ca>
2015-12-02 13:04:14 -0800

    Added missing headers for STM CSV file

--- lib/repositext/utils/subtitle_mark_tools.rb
+++ lib/repositext/utils/subtitle_mark_tools.rb
@@ -4,7 +4,7 @@ class Repositext
 
       # Returns array with headers for subtitle_markers CSV file
       def self.csv_headers
-        ['relativeMS', 'samples', 'charLength']
+        ['relativeMS', 'samples', 'charLength', 'persistentId', 'recordId']
       end
 
       # Returns just the body text of txt. Strips id_title and id_paragraph and
