7e1ee3c0891cf417665931e0e70a057aa65fe924
Jo Hund <jhund@clearcove.ca>
2016-01-04 15:49:50 -0800

    Refactored RFile:
    Moved generic methods higher up in the class hierarchy so that we can use them for binary files, too.

--- lib/repositext/r_file.rb
+++ lib/repositext/r_file.rb
@@ -11,6 +11,17 @@ class Repositext
              to: :repository,
              prefix: false
 
+    # Returns a relative path from source_path to target_path.
+    # @param source_path [String] absolute path
+    # @param target_path [String] absolute path
+    # @return [String]
+    def self.relative_path_from_to(source_path, target_path)
+      source_pathname = Pathname.new(source_path)
+      target_pathname = Pathname.new(target_path)
+      target_pathname.relative_path_from(source_pathname).to_s
+    end
+
+
     # @param contents [String] the file's contents as string
     # @param language [Language] the file's language
     # @param filename [String] the file's absolute path
@@ -36,6 +47,21 @@ class Repositext
     # @return [String]
     def dir
       File.dirname(filename)
+    end
+
+    # Returns self's date_code
+    def extract_date_code
+      basename.match(/\d{2}-\d{4}[[:alpha:]]?/).to_s
+    end
+
+    # Extracts a 4-digit product identity id from filename
+    def extract_product_identity_id
+      basename.match(/(?<=_)\d{4}(?=\.)/).to_s
+    end
+
+    # Extracts a 2-digit year from filename
+    def extract_year
+      extract_date_code.match(/\A\d{2}/).to_s
     end
 
     def inspect
