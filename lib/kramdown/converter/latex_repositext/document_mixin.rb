65ff37f30c9d983abe1b2e709a6fd7033b322cbc
Jo Hund <jhund@clearcove.ca>
2016-03-14 09:19:41 -0700

    PDF export: Fixed overfull hbox issue when including version-control-info:
    * The last column in the table (Commit message) was too wide and extended beyond the right page margin. I made it less wide and the issue went away.

--- lib/kramdown/converter/latex_repositext/document_mixin.rb
+++ lib/kramdown/converter/latex_repositext/document_mixin.rb
@@ -367,7 +367,7 @@ module Kramdown
           vcp << "\\begin{RtSubTitle}\nVersion Control Info\n\\end{RtSubTitle}\n"
           vcp << "\\relscale{0.66}\n"
           # commits table
-          vcp << "\\begin{tabular}{ | l | l | l | p{9cm} |}\n"
+          vcp << "\\begin{tabular}{ | l | l | l | p{8.3cm} |}\n"
           vcp << "\\hline\n"
           vcp << "\\textbf{Commit} & \\textbf{Author} & \\textbf{Date} & \\textbf{Commit message} \\\\ \n"
           vcp << "\\hline\n"
