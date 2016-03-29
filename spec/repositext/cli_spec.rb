2bd5735a48b0a3114980d8397cab7dd0d4519ab8
Jo Hund <jhund@clearcove.ca>
2016-03-29 09:11:16 -0700

    Updated Cli spec: Added `-content-type` argument

--- spec/repositext/cli_spec.rb
+++ spec/repositext/cli_spec.rb
@@ -8,7 +8,7 @@ class Repositext
     end
 
     it "prints help when invoked without subcommand or argument" do
-      out = capture_io { Cli.start(['--rtfile', nil]) }.join
+      out = capture_io { Cli.start(['--rtfile', nil, '--content-type', nil]) }.join
       out.must_match /Commands:\n/
     end
 
@@ -39,7 +39,9 @@ class Repositext
         validate
       ].each do |command|
         it "responds to '#{ command }'" do
-          out = capture_io { Cli.start([command, 'test', '--rtfile', nil]) }.join.strip
+          out = capture_io {
+            Cli.start([command, 'test', '--rtfile', nil, '--content-type', nil])
+          }.join.strip
           out.must_include "#{ command }_test"
         end
       end
@@ -51,7 +53,9 @@ class Repositext
         import
       ].each do |command|
         it "responds to '#{ command }'" do
-          out = capture_io { Cli.start([command, 'test', '--rtfile', nil, '--skip-git-up-to-date-check']) }.join.strip
+          out = capture_io {
+            Cli.start([command, 'test', '--rtfile', nil, '--content-type', nil, '--skip-git-up-to-date-check'])
+          }.join.strip
           out.must_include "#{ command }_test"
         end
       end
