c6107831482cef923ca829c6c8a2d923b9308766
Jo Hund <jhund@clearcove.ca>
2016-03-29 09:14:33 -0700

    Validator::IdmlRoundTrip: Fixed indentation

--- lib/repositext/validation/validator/idml_import_round_trip.rb
+++ lib/repositext/validation/validator/idml_import_round_trip.rb
@@ -12,42 +12,41 @@ class Repositext
           log_and_report_validation_step(outcome.errors, outcome.warnings)
         end
 
-      end
+      private
 
-    private
-
-      # @param [String] idml_file_contents
-      def valid_idml_round_trip?(idml_file_contents)
-        # parse Idml
-        idml_based_kramdown_doc = @options['idml_parser_class'].new(
-          idml_file_contents
-        ).parse
-        idml_based_kramdown_root = idml_based_kramdown_doc.root
-        # Serialize kramdown doc to kramdown string
-        idml_based_at_string = idml_based_kramdown_doc.send(
-          @options['kramdown_converter_method_name']
-        )
-        # Parse back the generated kramdown string
-        round_trip_kramdown_root = @options['kramdown_parser_class'].parse(
-          idml_based_at_string
-        ).first
-        # compare the two kramdown trees
-        diffs = idml_based_kramdown_root.compare_with(round_trip_kramdown_root)
-        if diffs.empty?
-          Outcome.new(true, nil)
-        else
-          Outcome.new(
-            false, nil, [],
-            diffs.map { |diff|
-              Reportable.error(
-                [@file_to_validate.path],
-                ['Roundtrip comparison results in different elements', diff]
-              )
-            }
+        # @param [String] idml_file_contents
+        def valid_idml_round_trip?(idml_file_contents)
+          # parse Idml
+          idml_based_kramdown_doc = @options['idml_parser_class'].new(
+            idml_file_contents
+          ).parse
+          idml_based_kramdown_root = idml_based_kramdown_doc.root
+          # Serialize kramdown doc to kramdown string
+          idml_based_at_string = idml_based_kramdown_doc.send(
+            @options['kramdown_converter_method_name']
           )
+          # Parse back the generated kramdown string
+          round_trip_kramdown_root = @options['kramdown_parser_class'].parse(
+            idml_based_at_string
+          ).first
+          # compare the two kramdown trees
+          diffs = idml_based_kramdown_root.compare_with(round_trip_kramdown_root)
+          if diffs.empty?
+            Outcome.new(true, nil)
+          else
+            Outcome.new(
+              false, nil, [],
+              diffs.map { |diff|
+                Reportable.error(
+                  [@file_to_validate.path],
+                  ['Roundtrip comparison results in different elements', diff]
+                )
+              }
+            )
+          end
         end
-      end
 
+      end
     end
   end
 end
