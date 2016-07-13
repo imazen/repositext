c3ffdb74a5b1c5de4f5a7894fed01f583093859a
Jo Hund <jhund@clearcove.ca>
2016-07-13 15:53:35 -0700

    Updated PDF export to reduce vspace below title text if last line contains a descender, e.g., a comma

--- spec/kramdown/converter/latex_repositext/document_mixin_spec.rb
+++ spec/kramdown/converter/latex_repositext/document_mixin_spec.rb
@@ -57,6 +57,22 @@ module Kramdown
           end
         end
 
+        describe "#compute_vspace_between_title_text_and_hrule" do
+          [
+            ["\\emph{no comma word word}", 6.733],
+            ["\\emph{comma on single line W\\RtSmCapsEmulation{ORD}, W\\RtSmCapsEmulation{ORD} W\\RtSmCapsEmulation{ORD}}", 3.1939999999999995],
+            ["\\emph{comma on last of multiple lines W\\RtSmCapsEmulation{ORD} W\\RtSmCapsEmulation{ORD} \\linebreak\n\\emph{W\\RtSmCapsEmulation{ORD}, W\\RtSmCapsEmulation{ORD}}", 3.1939999999999995],
+            ["\\emph{comma on first of multiple lines W\\RtSmCapsEmulation{ORD}, W\\RtSmCapsEmulation{ORD} \\linebreak\n\\emph{W\\RtSmCapsEmulation{ORD} W\\RtSmCapsEmulation{ORD}}", 6.733],
+          ].each do |test_string, xpect|
+            it "handles #{ test_string }" do
+              converter = LatexRepositextPlain.send(:new, '_', {})
+              converter.send(
+                :compute_vspace_between_title_text_and_hrule,
+                test_string
+              ).must_equal(xpect)
+            end
+          end
+        end
       end
     end
   end
