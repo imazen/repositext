3b2c28f2f662c790f96af86d12cdab284bd17678
Aaron Walker <aaron@branham.org>
2016-07-16 16:24:52 -0400

    Refacrtored RtTitle environment: * Added the use of strut rules to make the sapcing around the title consistent. * Adjusted the vspaces to work with the strut. * Adjusted the height of the hrules. * Added `%` in 2 places to ensure there was no leading or trailing whitespace.

--- lib/kramdown/converter/latex_repositext.rb
+++ lib/kramdown/converter/latex_repositext.rb
@@ -120,7 +120,7 @@ module Kramdown
           )
           # NOTE: We insert a percent sign immediately after l_title to prevent trailing whitespace
           # which would break the centering of the text.
-          "\\begin{RtTitle}\n#{ l_title }%\n\\end{RtTitle}"
+          "\\begin{RtTitle}%\n#{ l_title }%\n\\end{RtTitle}"
         when 2
           # render in RtTitle2 environment
           l_title = inner(el, opts)
