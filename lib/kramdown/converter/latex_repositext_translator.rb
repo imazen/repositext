86db76ecff75b1b2d7d7b15d69b1d2bdfbb90089
Aaron Walker <aaron@branham.org>
2016-07-25 09:53:49 -0400

    Updated apply_song_break_class to be a variable based off of is_primary_repo.

--- lib/kramdown/converter/latex_repositext_translator.rb
+++ lib/kramdown/converter/latex_repositext_translator.rb
@@ -5,12 +5,12 @@ module Kramdown
       include DocumentMixin
       include RenderSubtitleAndGapMarksMixin
 
-      # We ignore song_break behavior for translator PDFs so that translators
-      # clearly see where the stanza boundaries are, and aren't confused by page
-      # breaks inside a stanza. The tradeoff is that the spacing around songs
-      # in translator PDFs may be excessive.
+      # We ignore song_break behavior for English translator PDFs so that 
+      # translators clearly see where the stanza boundaries are when translating,
+      # and aren't confused by page breaks inside a stanza. The tradeoff is that
+      # the spacing around songs in English translator PDFs may be excessive.
       def apply_song_break_class
-        false
+        @options[:is_primary_repo] ? false : true
       end
 
     end
