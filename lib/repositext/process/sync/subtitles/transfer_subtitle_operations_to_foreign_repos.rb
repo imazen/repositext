c2f4e7bb5e2af748d79dbb0165ef3021985cd4d5
Jo Hund <jhund@clearcove.ca>
2016-08-17 12:09:31 -0700

    sync subtitles: Improve performance and reporting by not iterating over all content_types, and getting content_type from primary content AT file instead.

--- lib/repositext/process/sync/subtitles/transfer_subtitle_operations_to_foreign_repos.rb
+++ lib/repositext/process/sync/subtitles/transfer_subtitle_operations_to_foreign_repos.rb
@@ -37,8 +37,10 @@ class Repositext
             primary_files_flagged_for_manual_resolution,
             foreign_content_at_file_override=nil
           )
+            verbose_logging = true
             # Compute all data related to the to_primary_content_at_file
             to_primary_content_at_file = st_ops_for_file.content_at_file
+            primary_content_type = to_primary_content_at_file.content_type
             puts "   - primary file: #{ to_primary_content_at_file.basename }"
             to_stm_csv_file = to_primary_content_at_file.corresponding_subtitle_markers_csv_file
             to_subtitles = to_stm_csv_file.subtitles
@@ -61,66 +63,92 @@ class Repositext
               RepositorySet.new(@repository.parent_dir).all_repos(:foreign_content_repos)
             end
             foreign_repos.each do |foreign_repo|
+              if !foreign_repo.read_repo_level_data['st_sync_active']
+                # puts("     - #{ foreign_repo.name }: skip repository")  if verbose_logging
+                next
+              end
               foreign_files_flagged_for_manual_resolution = []
-              # Iterate over all content_types or just the override
-              foreign_content_types = if foreign_content_at_file_override
-                # Use override
-                [foreign_content_at_file_override.content_type]
-              else
-                # Use all foreign content types
-                ContentType.all(foreign_repo)
-              end
-              foreign_content_types.each do |foreign_content_type|
-                # find corresponding foreign content_at file
-                foreign_content_at_file = (
-                  foreign_content_at_file_override ||
-                  RFile::ContentAt.find_by_product_identity_id(
-                    to_primary_content_at_file.extract_product_identity_id,
-                    foreign_content_type
-                  )
-                )
-                next  if foreign_content_at_file.nil?
-                print "     - #{ foreign_repo.name }:"
-                if !foreign_content_at_file.has_subtitle_marks?
-                  puts " skipping, has no subtitle marks"
-                  next
-                end
-                # next  if foreign_content_at_file has pending import
-                if foreign_content_at_file.read_file_level_data['exported_subtitles_at_st_sync_commit']
-                  puts " skipping, has pending subtitle import"
-                  next
-                end
+              print "     - #{ foreign_repo.name }: "
 
-                # Determine strategy for transferring st_ops
-                transfer_strategy = determine_st_ops_transfer_strategy(
-                  foreign_content_at_file: foreign_content_at_file,
-                  from_git_commit: @from_git_commit,
-                  from_subtitles_count: from_subtitles_count,
-                  st_ops_count_delta: st_ops_count_delta,
-                  to_subtitles_count: to_subtitles_count,
-                )
-                case transfer_strategy
-                when :apply_st_ops_as_is_initial, :apply_st_ops_as_is_subsequent
-                  apply_st_ops_as_is!(
-                    st_ops_for_file,
-                    from_subtitles,
-                    to_subtitles,
-                    foreign_content_at_file,
-                    st_review_flags
-                  )
-                when :resolve_manually
-                  foreign_files_flagged_for_manual_resolution << foreign_content_at_file
-                  primary_files_flagged_for_manual_resolution << to_primary_content_at_file
-                when :resolve_mismatch_in_subtitle_counts
-                  foreign_files_flagged_for_manual_resolution << foreign_content_at_file
-                  primary_files_flagged_for_manual_resolution << to_primary_content_at_file
-                when :resolve_inconsistent_git_history
-                  foreign_files_flagged_for_manual_resolution << foreign_content_at_file
-                  primary_files_flagged_for_manual_resolution << to_primary_content_at_file
-                else
-                  raise "Handle this: #{ transfer_strategy.inspect }"
-                end
+              # Compute foreign content_type
+              foreign_content_type = if foreign_content_at_file_override
+                # Use content_type from file override
+                foreign_content_at_file_override.content_type
+              else
+                # Use primary's corresponding foreign content type
+                primary_content_type.corresponding_content_type_in_other_repo(foreign_repo)
               end
+
+              # find corresponding foreign content_at file
+              foreign_content_at_file = (
+                foreign_content_at_file_override ||
+                RFile::ContentAt.find_by_product_identity_id(
+                  to_primary_content_at_file.extract_product_identity_id,
+                  foreign_content_type
+                )
+              )
+
+              # Skip file under a number of conditions
+              if foreign_content_at_file.nil?
+                puts(verbose_logging ? "skip, corresponding file does not exist" : '')
+                next
+              end
+              if foreign_content_at_file.read_file_level_data['exported_subtitles_at_st_sync_commit']
+                puts(verbose_logging ? "skip, has pending subtitle import" : '')
+                next
+              end
+              if foreign_content_at_file.read_file_level_data['st_sync_commit'] == @to_git_commit
+                puts(verbose_logging ? "skip, subtitles are already synced" : '')
+                next
+              end
+
+              # Determine strategy for transferring st_ops
+              transfer_strategy = determine_st_ops_transfer_strategy(
+                foreign_content_at_file: foreign_content_at_file,
+                from_git_commit: @from_git_commit,
+                from_subtitles_count: from_subtitles_count,
+                st_ops_count_delta: st_ops_count_delta,
+                to_subtitles_count: to_subtitles_count,
+              )
+              case transfer_strategy
+              when :apply_initial_custom_st_ops
+                # First sync to foreign file, file already contains subtitles.
+                # Determine what version of primary subtitles are based on
+                # and sync from there.
+                print "apply initial custom st-ops"
+                # TBD
+                puts " - TBD".color(:yellow)
+              when :apply_subsequent_st_ops_as_is
+                # Subsequent sync to foreign file. All versions match, apply
+                # st-ops as is.
+                print " apply subsequent st-ops as is"
+                apply_st_ops_as_is!(
+                  st_ops_for_file,
+                  from_subtitles,
+                  to_subtitles,
+                  foreign_content_at_file,
+                  st_review_flags
+                )
+                puts " - OK".color(:green)
+              when :auto_split_initial_subtitles
+                # First sync to foreign file. File contains no subtitles,
+                # so we auto split subtitles based on current primary and
+                # flag file for review.
+                print "auto split initial subtitles"
+                # TBD
+                puts " - TBD".color(:yellow)
+              when :resolve_inconsistent_git_history
+                puts "resolve inconsistent git history"
+                foreign_files_flagged_for_manual_resolution << foreign_content_at_file
+                primary_files_flagged_for_manual_resolution << to_primary_content_at_file
+              when :resolve_manually
+                puts "resolve manually"
+                foreign_files_flagged_for_manual_resolution << foreign_content_at_file
+                primary_files_flagged_for_manual_resolution << to_primary_content_at_file
+              else
+                raise "Handle this: #{ transfer_strategy.inspect }"
+              end
+
               # We don't want to finalize the repo if we just transferred
               # st ops for a single file via override...
               if !foreign_content_at_file_override
