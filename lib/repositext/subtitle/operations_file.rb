5b7e7162a5dba9a4c4af7883bc85f7ba1f30bbd1
Jo Hund <jhund@clearcove.ca>
2016-08-22 12:19:05 -0700

    Updated Subtitle::OperationsFile:
    * added methods to support subtitle sync
    * refactored truncating of git commit SHA1s into method
    * merged .get_all_sync_commits into .get_sync_commits

--- lib/repositext/subtitle/operations_file.rb
+++ lib/repositext/subtitle/operations_file.rb
@@ -10,15 +10,35 @@ class Repositext
     #     "st-ops-2016_07_12-20_39_28-123456-to-654321.json"
     class OperationsFile
 
-      # Returns a string to be used in st-ops file names to document the sync commits.
+      # Returns true if any of the st-ops files in st_ops_dir are bounded by
+      # git_commit_sha1
+      # @param st_ops_dir [String]
+      # @param git_commit_sha1 [String]
+      # @return [Boolean]
+      def self.any_with_git_commit?(st_ops_dir, git_commit_sha1)
+        gcsha1 = truncate_git_commit_sha1(git_commit_sha1)
+        Dir.glob(File.join(st_ops_dir, "st-ops-*-#{ gcsha1 }*.json")).any?
+      end
+
+      # Returns the SHA1 of the earliest `from_git_commit` in st_ops_dir
+      # @param st_ops_dir [String]
+      # @return [String]
+      def self.compute_earliest_from_commit(st_ops_dir)
+        earliest_st_ops_file_name = find_latest(st_ops_dir)
+        return nil  if earliest_st_ops_file_name.nil?
+        extract_from_and_to_commit_from_filename(earliest_st_ops_file_name).first
+      end
+
+      # Returns a string to be used in st-ops file names to document the sync
+      # commits. Example: "123456-to-654321"
       # @param from_commit [String] sha1 hash of from-commit
       # @param to_commit [String] sha1 hash of to-commit
       # @return [String]
       def self.compute_from_to_git_commit_marker(from_commit, to_commit)
         [
-          from_commit.first(6),
+          truncate_git_commit_sha1(from_commit),
           '-to-',
-          to_commit.first(6),
+          truncate_git_commit_sha1(to_commit),
         ].join
       end
 
@@ -105,29 +125,26 @@ class Repositext
         Dir.glob(File.join(st_ops_dir, "st-ops-*.json"))
       end
 
-      def self.get_all_sync_commits(st_ops_dir)
-        get_all_st_ops_files(st_ops_dir).map { |e|
-          extract_from_and_to_commit_from_filename(e)
-        }.flatten.uniq.map { |e| e.downcase }
-      end
-
-      # Returns array of sync commits based files found in the st-ops folder.
-      # If start_with_commit is given, returns all sync commits starting with
-      # this one to the latest one.
+      # Returns array of sync commits based on files found in the st-ops folder.
+      # start_with_commit can optionally be specified and defaults to the very
+      # first `from` commit.
       # @param st_ops_dir [String] path to the folder containing st-ops files
       # @param start_with_commit [String, optional]
       # @return [Array<String>] first 6 characters of each commit's sha1
       def self.get_sync_commits(st_ops_dir, start_with_commit=nil)
-        all_sync_commits = get_all_sync_commits(st_ops_dir)
+        all_sync_commits = get_all_st_ops_files(st_ops_dir).map { |e|
+          extract_from_and_to_commit_from_filename(e)
+        }.flatten.uniq
+
         if start_with_commit
-          sanitized_start_with_commit = start_with_commit.first(6).downcase
-          include_cur_commit = false
+          sanitized_start_with_commit = truncate_git_commit_sha1(start_with_commit)
+          include_following_commits = false
           r = all_sync_commits.find_all { |e|
-            # Find all commits starting with and following start_with_commit
-            include_cur_commit ||= (e == sanitized_start_with_commit)
+            # Find all commits from start_with_commit forward
+            include_following_commits ||= (e == sanitized_start_with_commit)
           }
           if r.empty?
-            raise "Could not find start commit #{ start_with_commit.inspect } in #{ all_sync_commits.inspect }"
+            raise "Could not find start commit #{ start_with_commit.inspect }.".color(:red)
           end
         else
           r = all_sync_commits
@@ -135,6 +152,12 @@ class Repositext
         r
       end
 
+      # Returns truncated copy of gc_sha1 fit for st-ops file names.
+      # @param gc_sha1 [String]
+      # @return [String]
+      def self.truncate_git_commit_sha1(gc_sha1)
+        gc_sha1.first(6)
+      end
     end
   end
 end
