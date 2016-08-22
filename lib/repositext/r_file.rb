91c50be8cdff9a7dec2e11821402d511332486cd
Jo Hund <jhund@clearcove.ca>
2016-08-22 12:14:39 -0700

    Updated RFile:
    * #as_of_git_commit: added commit_reference argument (:at_commit, :at_next_commit)
    * added #latest_git_commit
    * added #reload_contents!

--- lib/repositext/r_file.rb
+++ lib/repositext/r_file.rb
@@ -65,19 +65,56 @@ class Repositext
       @content_type = content_type
     end
 
-    # Returns copy of self with contents as of git_commit_sha1
+    # Returns copy of self with contents as of a commit relative to git_commit_sha1.
+    # reference can be one of:
+    #   * :at_commit - as of the commit.
+    #   * :at_next_commit - as of next commit that included self, or current
+    #     contents if there is no next commit.
     # @param git_commit_sha1 [String]
-    def as_of_git_commit(git_commit_sha1)
+    # @return [RFile]
+    def as_of_git_commit(git_commit_sha1, reference=:at_commit)
+      if !git_commit_sha1.is_a?(String)
+        raise ArgumentError.new("Invalid git_commit_sha1: #{ git_commit_sha1.inspect }")
+      end
       if '' == git_commit_sha1.to_s
         raise ArgumentError.new("Invalid git_commit_sha1: #{ git_commit_sha1.inspect }")
       end
       # Instantiate copy of self with contents as of the requested version
-      self.class.new(
-        `git --no-pager show #{ git_commit_sha1 }:#{ repo_relative_path }`,
-        language,
-        filename,
-        content_type
-      )
+      new_contents = case reference
+      when :at_commit
+        # Use git_commit_sha1
+        `git --no-pager show #{ git_commit_sha1 }:#{ repo_relative_path }`
+      when :at_next_commit
+        # Find next commit's sha1
+        # Get all commits between git_commit_sha1 and HEAD in reverse order,
+        # starting with the next one after git_commit_sha1, ending with HEAD,
+        # one commit sha1 per line.
+        all_commit_sha1s, _ = Open3.capture2(
+          [
+            "git",
+            "--git-dir=#{ repository.repo_path }",
+            "log",
+            "--reverse",
+            "--pretty=format:'%H'",
+            "--ancestry-path",
+            "#{git_commit_sha1}..HEAD",
+            "--",
+            filename.sub(repository.base_dir, ''),
+          ].join(' ')
+        )
+        next_commit_sha1 = all_commit_sha1s.lines.first
+        if '' != next_commit_sha1.to_s
+          # Load contents as of next commit
+          `git --no-pager show #{ next_commit_sha1 }:#{ repo_relative_path }`
+        else
+          # No next commit, use current file contents
+          is_binary ? File.binread(filename) : File.read(filename)
+        end
+      else
+        raise "Handle this: #{ reference.inspect }"
+      end
+      # Return new instance of self
+      self.class.new(new_contents, language, filename, content_type)
     end
 
     # Returns just the name without path
@@ -109,6 +146,15 @@ class Repositext
       language.code_3_chars
     end
 
+    # Returns the latest git commit that included self. Before_time is optional
+    # and defaults to now.
+    # @param before_time [Time, optional]
+    # @return [Rugged::Commit]
+    def latest_git_commit(before_time=nil)
+      return nil  if repository.nil?
+      repository.latest_commit(filename, before_time)
+    end
+
     # Lock self for a block so only one process can modify it at a time.
     # NOTE: This is from Rails' File Cache:
     # https://github.com/rails/rails/blob/932655a4ef61083da98724bb612d00f89e153c46/activesupport/lib/active_support/cache/file_store.rb#L103
@@ -127,6 +173,11 @@ class Repositext
       else
         yield
       end
+    end
+
+    # Reloads contents from filename
+    def reload_contents!
+      @contents = File.read(filename)
     end
 
     # Returns relative path from repo root to self
