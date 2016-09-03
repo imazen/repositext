ad3619d12be5741a4792504f28d6c085cbbbc853
Jo Hund <jhund@clearcove.ca>
2016-09-03 10:41:19 -0700

    Updated sync subtitles: On initial sync process all primary files, not just the ones with changes.
    This is so that they all have the correct sync commit in their data.json file.

--- lib/repositext/process/compute/subtitle_operations_for_repository.rb
+++ lib/repositext/process/compute/subtitle_operations_for_repository.rb
@@ -33,12 +33,68 @@ class Repositext
             )
           end
 
+          operations_for_all_files = if is_initial_sync?
+            process_all_primary_files
+          else
+            process_primary_files_with_changes_only
+          end
+
+          ofr = Repositext::Subtitle::OperationsForRepository.new(
+            {
+              repository: @repository.name,
+              from_git_commit: @from_git_commit,
+              to_git_commit: @to_git_commit,
+            },
+            operations_for_all_files
+          )
+
+          ofr
+        end
+
+        # Returns array with operations for all primary files
+        def process_all_primary_files
+          Dir.glob(
+            File.join(@repository.base_dir, '**/content/**/*.at')
+          ).each { |absolute_file_path|
+            next nil  if !@file_list.any? { |e| absolute_file_path.index(e) }
+            # Skip non content_at files
+            unless absolute_file_path =~ /\/content\/.+\d{4}\.at\z/
+              raise "shouldn't get here"
+            end
+
+            content_at_file_to = Repositext::RFile::ContentAt.new(
+              File.read(absolute_file_path),
+              @language,
+              absolute_file_path,
+              @content_type
+            )
+
+            puts "     - process #{ content_at_file_to.repo_relative_path }"
+
+            soff = SubtitleOperationsForFile.new(
+              content_at_file_to,
+              @repository.base_dir,
+              {
+                from_git_commit: @from_git_commit,
+                to_git_commit: @to_git_commit,
+              }
+            ).compute
+
+            # Return nil if no subtitle operations exist for this file
+            soff.operations.any? ? soff : nil
+          }.compact
+        end
+
+        # Returns array with operations for primary files that changed only
+        def process_primary_files_with_changes_only
           # We get the diff only so that we know which files have changed.
           diff = @repository.diff(@from_git_commit, @to_git_commit, context_lines: 0)
 
           operations_for_all_files = diff.patches.map { |patch|
             file_name = patch.delta.old_file[:path]
             next nil  if !@file_list.include?(file_name)
+
+            # next nil  if !file_name.index('63-0728')
 
             # Skip non content_at files
             unless file_name =~ /\/content\/.+\d{4}\.at\z/
@@ -67,21 +123,12 @@ class Repositext
             # Return nil if no subtitle operations exist for this file
             soff.operations.any? ? soff : nil
           }.compact
-
-          ofr = Repositext::Subtitle::OperationsForRepository.new(
-            {
-              repository: @repository.name,
-              from_git_commit: @from_git_commit,
-              to_git_commit: @to_git_commit,
-            },
-            operations_for_all_files
-          )
-
-          ofr
         end
 
+        def is_initial_sync?
+          true
+        end
       end
-
     end
   end
 end
