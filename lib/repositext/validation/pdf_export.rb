1cc814b6d94a2127c582341c59782fc105ce96a0
Jo Hund <jhund@clearcove.ca>
2016-10-10 15:08:41 -0700

    PDF export validation: Skip previously exported files that have title in filename already.

--- lib/repositext/validation/pdf_export.rb
+++ lib/repositext/validation/pdf_export.rb
@@ -5,6 +5,8 @@ class Repositext
       # Specifies validations to run related to PDF export.
       def run_list
         validate_files(:exported_pdfs) do |path|
+          next  if path.index('.--') # skip files that have title added to filename. Those are from a previous export.
+
           Validator::PdfExportConsistency.new(
             path, @logger, @reporter, @options
           ).run
