7eeab0ce1bd75dd0c2c8dc2ed75df6d446e43fbd
Jo Hund <jhund@clearcove.ca>
2016-10-12 13:52:19 -0700

    Added Docx import validation: validate_document_styles_not_modified

--- lib/repositext/validation/validator/docx_import_workflow.rb
+++ lib/repositext/validation/validator/docx_import_workflow.rb
@@ -45,7 +45,11 @@ class Repositext
           validate_correct_file_name(docx_file.basename, errors, warnings)
           validate_corresponding_content_at_with_subtitles_does_not_exist(docx_file, errors, warnings)
           # validate_correct_template_file_used(docx_file, errors, warnings)
-          # validate_document_styles_not_modified(docx_file, errors, warnings)
+          validate_document_styles_not_modified(
+            docx_file.extract_docx_styles_xml,
+            errors,
+            warnings
+          )
 
           Outcome.new(errors.empty?, nil, [], errors, warnings)
         end
@@ -110,8 +114,148 @@ class Repositext
         # (e.g., to automatically make all scripture italic.)
         # They need to apply any text formatting manually (e.g., italic) as
         # character attributes.
-        def validate_document_styles_not_modified(docx_file, errors, warnings)
-          # TBD
+        # @param styles_xml_contents [String] the contents of word/styles.xml as string
+        # @param errors [Array] collector for errors
+        # @param warnings [Array] collector for warnings
+        def validate_document_styles_not_modified(styles_xml_contents, errors, warnings)
+          xml_styles = Nokogiri::XML(styles_xml_contents) { |config| config.noblanks }
+          xml_styles.xpath('//w:styles//w:style').each { |style_xn|
+            style_id =  style_xn['w:styleId']
+            if (expected_style_attrs = expected_styles[style_id])
+              # We check attrs for this style_id
+
+              # Check basedOn
+              exp_bo = expected_style_attrs[:basedOn]
+              if(
+                # We expect basedOn to be present
+                exp_bo &&
+                (
+                  (act_bo_xn = style_xn.at_xpath('./w:basedOn')).nil? ||
+                  (act_bo = act_bo_xn['w:val']) != exp_bo
+                )
+              ) || (
+                # We expect basedOn to be absent
+                !exp_bo &&
+                (act_bo_xn = style_xn.at_xpath('./w:basedOn')) &&
+                (act_bo = act_bo_xn['w:val'].to_s) != ''
+              )
+                errors << Reportable.error(
+                  [
+                    @file_to_validate,
+                  ],
+                  [
+                    'Unexpected modification of document styles',
+                    "Style: #{ style_id }, expected basedOn to be #{ exp_bo.inspect }, got #{ act_bo.inspect }"
+                  ]
+                )
+              end
+
+              # Check pPr (paragraph styles)
+              exp_ppr = expected_style_attrs[:pPr]
+              act_ppr_xn = style_xn.at_xpath('./w:pPr')
+              act_ppr = act_ppr_xn ? act_ppr_xn.children.map { |e| e.name }.sort : []
+              if(
+                # We expect pPr to be present
+                exp_ppr && act_ppr != exp_ppr
+              ) || (
+                # We expect pPr to be absent
+                !exp_ppr && act_ppr  != []
+              )
+                errors << Reportable.error(
+                  [
+                    @file_to_validate,
+                  ],
+                  [
+                    'Unexpected modification of document styles',
+                    "Style: #{ style_id }, expected pPr to contain the following attrs: #{ exp_ppr.inspect }, got #{ act_ppr.inspect }"
+                  ]
+                )
+              end
+
+              # Check rPr (text run styles)
+              exp_rpr = expected_style_attrs[:rPr]
+              act_rpr_xn = style_xn.at_xpath('./w:rPr')
+              act_rpr = act_rpr_xn ? act_rpr_xn.children.map { |e| e.name }.sort : []
+              if(
+                # We expect pPr to be present
+                exp_rpr && act_rpr != exp_rpr
+              ) || (
+                # We expect pPr to be absent
+                !exp_rpr && act_rpr != []
+              )
+                errors << Reportable.error(
+                  [
+                    @file_to_validate,
+                  ],
+                  [
+                    'Unexpected modification of document styles',
+                    "Style: #{ style_id }, expected pPr to contain the following attrs: #{ exp_rpr.inspect }, got #{ act_rpr.inspect }"
+                  ]
+                )
+              end
+            end
+          }
+        end
+
+        # Returns a hash with expected attributes for the styles we validate.
+        # The document styles must have exactly the expected attributes, no
+        # more and no less. This is to ensure that text formatting like italics
+        # and bold is applied via character attributes and not as part of the
+        # paragraph style.
+        # This implementation matches the following two styles:
+        #     <w:style w:default="1" w:styleId="Normal" w:type="paragraph">
+        #       <w:name w:val="Normal"/>
+        #       <w:rsid w:val="0007621A"/>
+        #       <w:pPr>
+        #         <w:tabs>
+        #           <w:tab w:pos="360" w:val="left"/>
+        #         </w:tabs>
+        #         <w:spacing w:after="0" w:before="60" w:line="240" w:lineRule="auto"/>
+        #         <w:jc w:val="both"/>
+        #       </w:pPr>
+        #       <w:rPr>
+        #         <w:rFonts w:ascii="V-Excelsior LT Std" w:hAnsi="V-Excelsior LT Std"/>
+        #         <w:color w:themeColor="text1" w:val="000000"/>
+        #         <w:sz w:val="20"/>
+        #       </w:rPr>
+        #     </w:style>
+        #     <w:style w:styleId="Heading1" w:type="paragraph">
+        #       <w:name w:val="heading 1"/>
+        #       <w:basedOn w:val="Normal"/>
+        #       <w:next w:val="Normal"/>
+        #       <w:link w:val="Heading1Char"/>
+        #       <w:uiPriority w:val="9"/>
+        #       <w:rsid w:val="00A06EAA"/>
+        #       <w:pPr>
+        #         <w:keepNext/>
+        #         <w:keepLines/>
+        #         <w:spacing w:before="480"/>
+        #         <w:outlineLvl w:val="0"/>
+        #       </w:pPr>
+        #       <w:rPr>
+        #         <w:rFonts w:asciiTheme="majorHAnsi" w:cstheme="majorBidi" w:eastAsiaTheme="majorEastAsia" w:hAnsiTheme="majorHAnsi"/>
+        #         <w:b/>
+        #         <w:bCs/>
+        #         <w:color w:themeColor="accent1" w:themeShade="B5" w:val="345A8A"/>
+        #         <w:sz w:val="32"/>
+        #         <w:szCs w:val="32"/>
+        #       </w:rPr>
+        #     </w:style>
+        # @return [Hash] with style ids as keys
+        def expected_styles
+          # Important: Sort attrs under pPr and rPr alphabetically!
+          {
+            "Normal" => {
+              basedOn: nil,
+              pPr: %w[jc spacing tabs],
+              rPr: %w[color rFonts sz],
+            },
+            "Heading1" => {
+              basedOn: "Normal",
+              pPr: %w[keepLines keepNext outlineLvl spacing],
+              rPr: %w[b bCs color rFonts sz szCs],
+            },
+          }
         end
 
       end
--- spec/repositext/validation/validator/docx_import_workflow_spec.rb
+++ spec/repositext/validation/validator/docx_import_workflow_spec.rb
@@ -46,6 +46,134 @@ class Repositext
           end
         end
 
+        describe '#validate_document_styles_not_modified' do
+
+          let(:styles_xml_prefix) {
+            %(
+              <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+              <w:styles mc:Ignorable="w14" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" xmlns:w14="http://schemas.microsoft.com/office/word/2010/wordml">
+            )
+          }
+          let(:styles_xml_suffix) { %(</w:styles>) }
+
+          valid_styles = {
+            'Normal' => %(
+              <w:style w:default="1" w:styleId="Normal" w:type="paragraph">
+                <w:name w:val="Normal"/>
+                <w:rsid w:val="0007621A"/>
+                <w:pPr>
+                  <w:tabs>
+                    <w:tab w:pos="360" w:val="left"/>
+                  </w:tabs>
+                  <w:spacing w:after="0" w:before="60" w:line="240" w:lineRule="auto"/>
+                  <w:jc w:val="both"/>
+                </w:pPr>
+                <w:rPr>
+                  <w:rFonts w:ascii="V-Excelsior LT Std" w:hAnsi="V-Excelsior LT Std"/>
+                  <w:color w:themeColor="text1" w:val="000000"/>
+                  <w:sz w:val="20"/>
+                </w:rPr>
+              </w:style>
+            ),
+            'Heading1' => %(
+              <w:style w:styleId="Heading1" w:type="paragraph">
+                <w:name w:val="heading 1"/>
+                <w:basedOn w:val="Normal"/>
+                <w:next w:val="Normal"/>
+                <w:link w:val="Heading1Char"/>
+                <w:uiPriority w:val="9"/>
+                <w:rsid w:val="00A06EAA"/>
+                <w:pPr>
+                  <w:keepNext/>
+                  <w:keepLines/>
+                  <w:spacing w:before="480"/>
+                  <w:outlineLvl w:val="0"/>
+                </w:pPr>
+                <w:rPr>
+                  <w:rFonts w:asciiTheme="majorHAnsi" w:cstheme="majorBidi" w:eastAsiaTheme="majorEastAsia" w:hAnsiTheme="majorHAnsi"/>
+                  <w:b/>
+                  <w:bCs/>
+                  <w:color w:themeColor="accent1" w:themeShade="B5" w:val="345A8A"/>
+                  <w:sz w:val="32"/>
+                  <w:szCs w:val="32"/>
+                </w:rPr>
+              </w:style>
+            )
+          }
+
+          [
+            [
+              'Correct style attrs',
+              valid_styles['Heading1'],
+              [],
+            ],
+            [
+              'Incorrect basedOn',
+              valid_styles['Heading1'].sub(%(<w:basedOn w:val="Normal"/>), %(<w:basedOn w:val="Incorrect"/>)),
+              [["Unexpected modification of document styles", "Style: Heading1, expected basedOn to be \"Normal\", got \"Incorrect\""]],
+            ],
+            [
+              'Missing basedOn',
+              valid_styles['Heading1'].sub(%(<w:basedOn w:val="Normal"/>), %()),
+              [["Unexpected modification of document styles", "Style: Heading1, expected basedOn to be \"Normal\", got nil"]],
+            ],
+            [
+              'Unexpected basedOn',
+              valid_styles['Normal'].sub(%(<w:name w:val="Normal"/>), %(<w:name w:val="Normal"/><w:basedOn w:val="Unexpected"/>)),
+              [["Unexpected modification of document styles", "Style: Normal, expected basedOn to be nil, got \"Unexpected\""]],
+            ],
+            [
+              'Incorrect pPr (added attr)',
+              valid_styles['Normal'].sub(%(<w:jc w:val="both"/>), %(<w:jc w:val="both"/><w:keepNext/>)),
+              [["Unexpected modification of document styles", "Style: Normal, expected pPr to contain the following attrs: [\"jc\", \"spacing\", \"tabs\"], got [\"jc\", \"keepNext\", \"spacing\", \"tabs\"]"]],
+            ],
+            [
+              'Incorrect pPr (removed attr)',
+              valid_styles['Normal'].sub(%(<w:jc w:val="both"/>), %()),
+              [["Unexpected modification of document styles", "Style: Normal, expected pPr to contain the following attrs: [\"jc\", \"spacing\", \"tabs\"], got [\"spacing\", \"tabs\"]"]],
+            ],
+            [
+              'Missing pPr',
+              valid_styles['Normal'].gsub("w:pPr", 'w:pPrX'),
+              [["Unexpected modification of document styles", "Style: Normal, expected pPr to contain the following attrs: [\"jc\", \"spacing\", \"tabs\"], got []"]],
+            ],
+            [
+              'Incorrect rPr (added attr)',
+              valid_styles['Normal'].sub(%(<w:sz w:val="20"/>), %(<w:sz w:val="20"/><w:keepNext/>)),
+              [["Unexpected modification of document styles", "Style: Normal, expected pPr to contain the following attrs: [\"color\", \"rFonts\", \"sz\"], got [\"color\", \"keepNext\", \"rFonts\", \"sz\"]"]],
+            ],
+            [
+              'Incorrect rPr (removed attr)',
+              valid_styles['Normal'].sub(%(<w:sz w:val="20"/>), %()),
+              [["Unexpected modification of document styles", "Style: Normal, expected pPr to contain the following attrs: [\"color\", \"rFonts\", \"sz\"], got [\"color\", \"rFonts\"]"]],
+            ],
+            [
+              'Missing rPr',
+              valid_styles['Normal'].gsub("w:rPr", 'w:rPrX'),
+              [["Unexpected modification of document styles", "Style: Normal, expected pPr to contain the following attrs: [\"color\", \"rFonts\", \"sz\"], got []"]],
+            ],
+          ].each do |desc, style_entries, xpect|
+            it "handles #{ desc }" do
+              validator = DocxImportWorkflow.new(
+                FileLikeStringIO.new('/_', '_'),
+                logger,
+                reporter,
+                {}
+              )
+              styles_xml_contents = [styles_xml_prefix, style_entries, styles_xml_suffix].join
+              errors = []
+              warnings = []
+              validator.send(
+                :validate_document_styles_not_modified,
+                styles_xml_contents,
+                errors,
+                warnings
+              )
+              errors.map { |e| e.details }.must_equal(xpect)
+            end
+          end
+        end
+
       end
 
     end
