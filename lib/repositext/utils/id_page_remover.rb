e826352e659a23915372e53eeabe6e93e80a6de9
Jo Hund <jhund@clearcove.ca>
2017-02-19 11:00:06 -0800

    Updated Utils::IdPageRemover:
    Implemented functionality where it returns the removed id_page

--- lib/repositext/utils/id_page_remover.rb
+++ lib/repositext/utils/id_page_remover.rb
@@ -7,17 +7,17 @@ class Repositext
       # @param txt [String] complete AT document
       # @return [Array<String>] An array of content AT without id page, and the id page
       def self.remove(txt)
-        txt.gsub(
-          /
-            [^\n]+\n # the line before a line that contains '.id_title1'
-            [^\n]+\.id_title1 # line that contains id_title
-            .* # anything after the line that contains .id_title
-          /mx, # multiline so that the last .* matches multiple lines to the end of file
-          ''
-        )
+        id_page_regexp = /
+          [^\n]+\n # the line before a line that contains '.id_title1'
+          [^\n]+\.id_title1 # line that contains id_title
+          .* # anything after the line that contains .id_title
+        /mx # multiline so that the last .* matches multiple lines to the end of file
+
+        id_page = txt.match(id_page_regexp).to_s
+        txt_wo_id_page = txt.gsub(id_page_regexp, '')
+        [txt_wo_id_page, id_page]
       end
 
     end
   end
 end
-
