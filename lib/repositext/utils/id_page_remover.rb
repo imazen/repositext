7b833f8f890fed40bee9c0dad57a167f36b6f6b3
Jo Hund <jhund@clearcove.ca>
2017-06-30 13:45:28 -0700

    Made IdPageRemover more tolerant re formatting of .id_title1 IAL formatting.

--- lib/repositext/utils/id_page_remover.rb
+++ lib/repositext/utils/id_page_remover.rb
@@ -7,10 +7,13 @@ class Repositext
       # @param txt [String] complete AT document
       # @return [Array<String>] An array of content AT without id page, and the id page
       def self.remove(txt)
-        if(ial_pos = txt.index("\n{: .id_title1}"))
+        if(ial_pos = txt.index(".id_title1"))
           # txt contains id page, remove it
           # Find beginning of previous line
-          prev_line_start = txt.rindex("\n", ial_pos - 1) + 1
+          # Go back a few chars to eliminate the IALs preceding newline
+          # The step forward one char to go to beginning of previous line, not
+          # the newline of the previous line.
+          prev_line_start = txt.rindex("\n", ial_pos - 5) + 1
           [txt[0...prev_line_start], txt[prev_line_start..-1]]
         else
           # no id, return as is
