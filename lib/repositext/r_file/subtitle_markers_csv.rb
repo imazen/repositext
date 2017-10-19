27fd21496414eb0f7e26bcc7fee9e4e893ca956f
Jo Hund <jhund@clearcove.ca>
2017-10-18 17:48:35 -0700

    Added RFile::Csv class. STM CSV file inherits from it. Handle dependency boundaries.

--- lib/repositext/r_file/subtitle_markers_csv.rb
+++ lib/repositext/r_file/subtitle_markers_csv.rb
@@ -2,33 +2,12 @@ class Repositext
   class RFile
 
     # Represents a Subtitle marker CSV file in repositext.
-    class SubtitleMarkersCsv < RFile
+    class SubtitleMarkersCsv < Csv
 
       include FollowsStandardFilenameConvention
       include HasCorrespondingContentAtFile
       include HasCorrespondingPrimaryContentAtFile
       include HasCorrespondingPrimaryFile
-
-      # Options to be used when reading or writing CSV
-      def self.csv_options
-        { col_sep: "\t", headers: :first_row }
-      end
-
-      # Returns contents as CSV object.
-      # Important: This uses the current value of #contents (rather than
-      # reloading from disk!) so that this also works when we call, e.g.,
-      # `#subtitles` on an StmCsvFile that is checked out at a certain git commit.
-      def csv
-        CSV.new(contents, self.class.csv_options)
-      end
-
-      # Yields each row as hash with stringified keys to block
-      def each_row
-        csv.each do |row|
-          # row: #<CSV::Row "relativeMS":"6223" "samples":"151367170" "charLength":"34" "persistentId":"4498439" "recordId":"55020559">
-          yield(row)
-        end
-      end
 
       # Returns an array of the contained subtitles
       # @return [Array<Repositext::Subtitle>]
