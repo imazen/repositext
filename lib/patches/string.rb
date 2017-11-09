bf9f690071d00eb84160b18e34ce236659a7816f
Jo Hund <jhund@clearcove.ca>
2017-11-08 16:20:34 -0800

    Updated String#truncate_in_the_middle to allow skipping of truncation

--- lib/patches/string.rb
+++ lib/patches/string.rb
@@ -25,9 +25,9 @@ class String
 
   # Truncates self, removing the middle portion so that the beginning and
   # end are preserved.
-  # @param [Integer] max_len
+  # @param max_len [Integer] max length of return value. If 0, nothing will be truncated
   def truncate_in_the_middle(max_len)
-    r = if length > max_len
+    r = if(max_len > 0 && length > max_len)
       half_len = max_len/2
       "#{ self[0..half_len] } [...] #{ self[-half_len..-1] }"
     else
